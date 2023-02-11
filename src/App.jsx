1f86bf488537b0e1edbe2b3faa18f0a0c1406db5
Ali-Eldeba <alieldeba375@gmail.com>
2023-02-12 01:21:44 +0200

    adding basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+const StartPage = React.lazy(() => import("./pages/StartPage/StartPage"));
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+    </Routes>
+  );
 }
 
 export default React.memo(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import Loader from "./components/Loader";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <React.Suspense fallback={<Loader />}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.Suspense>
+    </Router>
   </React.StrictMode>
 );
